9fa79641cc45e066f188be664bb45a26d062c3e3
kwonhobaek <kwonhobaek@gmail.com>
2016-06-13 12:25:33 +0900

    .

---
+++ Exercise01.java
@@ -0,0 +1,82 @@
+
+
+
+public class Exercise01 {
+
+  static void exam() {
+
+    int randnum;
+
+    randnum = (int)(Math.random() * 10);
+
+    switch (randnum) {
+      case 0 :
+        System.out.println("000");
+        break;
+      case 1 :
+        System.out.println("111");
+        break;
+      case 2 :
+        System.out.println("222");
+        break;
+      case 3 :
+        System.out.println("333");
+        break;
+      case 4 :
+        System.out.println("444");
+        break;
+      case 5 :
+        System.out.println("555");
+        break;
+      case 6 :
+        System.out.println("666");
+        break;
+      case 7 :
+        System.out.println("777");
+        break;
+      case 8 :
+        System.out.println("888");
+        break;
+      case 9 :
+        System.out.println("999");
+        break;
+    }
+  }
+
+  static void answer(int a) {
+
+    switch (a) {
+      case 0 :
+        System.out.println("000");
+        break;
+      case 1 :
+        System.out.println("111");
+        break;
+      case 2 :
+        System.out.println("222");
+        break;
+      case 3 :
+        System.out.println("333");
+        break;
+      case 4 :
+        System.out.println("444");
+        break;
+      case 5 :
+        System.out.println("555");
+        break;
+      case 6 :
+        System.out.println("666");
+        break;
+      case 7 :
+        System.out.println("777");
+        break;
+      case 8 :
+        System.out.println("888");
+        break;
+      case 9 :
+        System.out.println("999");
+        break;
+    }
+  }
+
+}
---
+++ Test01.java
@@ -0,0 +1,11 @@
+
+
+public class Test01 {
+
+  public static void main(String[] args) {
+
+    Exercise01.exam();
+
+
+  }
+}

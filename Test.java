16a12307689eb137064b85e3e85a4bce9a682942
kwonhobaek <kwonhobaek@gmail.com>
2016-06-15 20:43:08 +0900

    0616

--- Test.java
+++ Test.java
@@ -4,7 +4,26 @@ public class Test {
 
   public static void main(String[] args) {
 
-  Exercise.exam();
+  // Exercise.exam();
+
+  char[] ch = args[0].toLowerCase().toCharArray();
+  int[] count = new int[26];
+
+  for (int i = 0; i < count.length; i++){
+    count[i] = 0;
+  }
+
+  for (int i = 0; i < ch.length; i++) {
+    count[ch[i] - 'a']++;
+
+  }
+
+  for (int i = 0; i < count.length; i++) {
+    if (count[i] <= 0)
+      continue;
+    System.out.printf("%c: %d \n", (char)(i + 'a'), count[i]);
+  }
+
 
 
   }

cf7ff89a67a33be5a2bf5393ca8a4823d8597ed8
baekkwonho <kwonhobaek@gmail.com>
2016-08-10 14:44:59 +0900

    .

--- src/Practice.java
+++ src/Practice.java
@@ -1,17 +1,26 @@
-import java.util.Scanner;
-
 public class Practice {
-  
+
   static int[] element = {3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48};
-  
+
   public static void main(String[] args) {
     
     print();
-   // bubbleSort(0, element.length - 1);
-    quickSort(0, element.length - 1);
-    
+    bubbleSort(0, element.length - 1);
     print();
+  }
+  
+  static void bubbleSort(int startIndex, int endIndex) {
+    if (endIndex <= startIndex)
+      return;
     
+    while (endIndex > 0) {
+      for (int i = startIndex; i < endIndex; i++) {
+        if (element[i] > element[i + 1]) {
+          swap(i, i + 1);
+        }
+      }
+      endIndex--;
+    }
     
   }
   
@@ -22,29 +31,15 @@ public class Practice {
     int pivotIndex = startIndex;
     int storeIndex = pivotIndex + 1;
     
-    for (int i = pivotIndex; i <= endIndex; i++) {
+    for (int i = pivotIndex + 1; i <= endIndex; i++) {
       if (element[i] < element[pivotIndex]) {
         swap(i, storeIndex);
         storeIndex++;
       }
     }
     swap(pivotIndex, storeIndex - 1);
-    quickSort(startIndex, storeIndex - 2);
+    quickSort(pivotIndex, storeIndex - 2);
     quickSort(storeIndex, endIndex);
-  }
-  
-  
-  static void bubbleSort(int startIndex, int endIndex) {
-    
-    while (endIndex > 0) {
-      for (int i = startIndex; i < endIndex; i++) {
-        if (element[i] > element[i + 1]) {
-          swap(i, i + 1);
-        }
-
-      }
-     endIndex--;
-    }
   }
   
   static void swap(int index1, int index2) {
@@ -56,13 +51,12 @@ public class Practice {
     element[index2] = temp;
   }
   
-  
-  
   static void print() {
     for (int value : element) {
       System.out.printf("%d ", value);
     }
     System.out.println();
   }
+  
 }
 
